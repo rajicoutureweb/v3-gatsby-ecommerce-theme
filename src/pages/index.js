86da82fa115d8aa8d264c5892462e26037cfbdf6
rajicoutureweb <138007442+rajicoutureweb@users.noreply.github.com>
2023-07-16 10:51:46 +0200

    Update index.js slogan

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,9 +30,9 @@ const IndexPage = () => {
       <Hero
         maxWidth={'500px'}
         image={'/banner1.png'}
-        title={'Essentials for a cold winter'}
-        subtitle={'Discover Autumn Winter 2021'}
-        ctaText={'shop now'}
+        title={'Style marocain, pure élégance'}
+        subtitle={'Découvrez la collection 2023'}
+        ctaText={'VOIR'}
         ctaAction={goToShop}
       />
 
