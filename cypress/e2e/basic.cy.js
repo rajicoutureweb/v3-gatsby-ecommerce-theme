cb164969b9540d4dc7346efcb0500dfebd6e28fe
rajicoutureweb <138007442+rajicoutureweb@users.noreply.github.com>
2023-07-16 11:10:35 +0200

    Update basic.cy.js slogan

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -1,10 +1,10 @@
-describe('sample render test', () => {
-  beforeEach(() => {
-    cy.visit('/')
-  })
+  describe('sample render test', () => {
+    beforeEach(() => {
+      cy.visit('/')
+    })
 
-  it('displays the title text', () => {
-    cy.get('h2')
-    .contains('Essentials for a cold winter');
-  })
-}) 
+    it('displays the title text', () => {
+      cy.get('h2')
+      .contains('Style marocain, pure élégance');
+    })
+  }) 
