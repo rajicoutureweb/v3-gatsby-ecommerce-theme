1a7ffa11c4ac7c2a1a96a8ae8fe8619e63bd7cb3
rajicoutureweb <138007442+rajicoutureweb@users.noreply.github.com>
2023-07-16 22:08:28 +0200

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -27,7 +27,7 @@ const Header = (prop) => {
   const [search, setSearch] = useState('');
 
   const searchRef = createRef();
-  const bannerMessage = 'Free shipping worldwide';
+  const bannerMessage = 'AMD';
   const searchSuggestions = [
     'Oversize sweaters',
     'Lama Pajamas',
