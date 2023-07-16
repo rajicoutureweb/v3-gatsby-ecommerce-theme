ed9304bfa17d13f46450c4d3dd3f6aff5bd0ec86
rajicoutureweb <138007442+rajicoutureweb@users.noreply.github.com>
2023-07-16 17:05:10 +0200

    line 25 :            {title && <h2 className={styles.title} m80mm>STYLE MAROCAIN, <minis>PURE ÉLÉGANCE</minis></h2>}

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -20,8 +20,9 @@ const Hero = (props) => {
     <div className={styles.root} style={{ backgroundImage: `url(${image})` }}>
       <div className={styles.content} style={{ maxWidth: '600px' }}>
         {header && <span className={styles.header}>{header}</span>}
-             <rectenglis>
-                    {title && <h2 className={styles.title} m80mm>{STYLE MAROCAIN, <minis>PURE ÉLÉGANCE</minis>}</h2>}
+  
+            <rectenglis>
+          {title && <h2 className={styles.title} m80mm>STYLE MAROCAIN, <minis>PURE ÉLÉGANCE</minis></h2>}
            </rectenglis>
 <center>
           
