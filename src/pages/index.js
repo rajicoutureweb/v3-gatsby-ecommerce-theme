bde3d2c31688809f8f99a2083bbb7a01ad86a65d
rajicoutureweb <138007442+rajicoutureweb@users.noreply.github.com>
2023-07-17 13:54:38 +0200

    TESTZchatGpTUpdate index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,7 @@ const IndexPage = () => {
       <Hero
         maxWidth={'500px'}
         image={'/banner1.png'}
-        title={'Style marocain, pure élégance'}
+       title={'Style marocain, <titleseconde>pure élégance</titleseconde>'}
         subtitle={'Découvrez la collection 2023'}
         ctaText={'VOIR'}
         ctaAction={goToShop}
